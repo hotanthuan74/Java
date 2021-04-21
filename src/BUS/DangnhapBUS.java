0daef9e44988263f07c526041050d85bdcb1bb15
thienv29 <thienv29@gmail.com>
2021-04-21 17:05:58 +0700

    sda

--- src/BUS/DangnhapBUS.java
+++ src/BUS/DangnhapBUS.java
@@ -28,33 +28,7 @@ public class DangnhapBUS {
         return list;
 
     }
-//    public boolean getPhanquyen(){
-//        for(DangnhapDTO dn:list){
-//            if(dn.getChucvunvDTO().equals("Admin")){
-//                return true;
-//            }
-//            
-//        }
-//        
-//    }
 
-//    public boolean chectAcountBUS(String tenDangnhap, String matkhau) {
-//
-//        try {
-//            DangnhapDTO dnDTO=new DangnhapDTO();
-//            if () {
-//
-//                System.out.println("Tìm thấy tài khoản thành công!");
-//                return true;
-//
-//            }
-//        } catch (Exception e) {
-//            System.out.println("Tìm thấy tài khoản thất bại!");
-//            return false;
-//        }
-//
-//        return false;
-//    }
      public DangnhapDTO getTaiKhoan(String tendn) {
         for (DangnhapDTO dnDTO : dnDAO.readAccountDAO()) {
             if (dnDTO.getTenDangnhapDTO().equals(tendn)) {
@@ -64,24 +38,7 @@ public class DangnhapBUS {
         }
         return null;
     }
-//     public DangnhapDTO getMatkhau(String matkhau)
-    
-//    public int dangnhap(String tenDangnhap,String matkhau) {
-//               
-//          if(dnDAO.readAccountDAO()!=null){
-//              if(dnDAO.checkAccount(tenDangnhap, matkhau)==true){
-//                  System.out.println("co");
-//                  if()
-//              }
-//              else if(dnDAO.checkAccount(tenDangnhap, matkhau)==false){
-//                  System.out.println("ko");
-//              }
-//          }
-//          return -1;
-//               
-//        
-//                
-//}
+
 
     public boolean addAccountBUS(DangnhapDTO dnDTO) {
         boolean rsesult = dnDAO.AddAccountDAO(dnDTO);
