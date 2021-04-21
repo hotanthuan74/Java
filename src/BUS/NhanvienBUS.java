000191343e36cdb0619e0332d7e6df86848f6cec
thienv29 <71214783+thienv29@users.noreply.github.com>
2021-04-21 16:38:19 +0700

    Add files via upload

--- src/BUS/NhanvienBUS.java
+++ src/BUS/NhanvienBUS.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package BUS;
-
+import DAO.NhanvienDAO;
+import DTO.NhanvienDTO;
+import java.util.ArrayList;
 /**
  *
  * @author Tan Thuan
  */
 public class NhanvienBUS {
-    
+    public boolean themNV(NhanvienDTO nv){
+        boolean result = new NhanvienDAO().themNV(nv);
+        return result;
+    }
+    public boolean xoaNV(NhanvienDTO nv){
+        boolean result = new NhanvienDAO().xoaNV(nv);
+        return result;
+    }
+    public boolean suaNV(NhanvienDTO nv){
+        boolean result = new NhanvienDAO().suaNV(nv);
+        return result;
+    }
+    public ArrayList<NhanvienDTO> getList(){
+        ArrayList<NhanvienDTO> listNV= new NhanvienDAO().getListNV();
+        
+        return listNV;
+    }
 }
