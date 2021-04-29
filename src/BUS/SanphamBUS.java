deecd4d0aa45aade985b0a5eb154c8e061b7f76c
thienv29 <thienv29@gmail.com>
2021-04-29 12:27:17 +0700

    s

--- src/BUS/SanphamBUS.java
+++ src/BUS/SanphamBUS.java
@@ -5,10 +5,33 @@
  */
 package BUS;
 
+import DAO.SanphamDAO;
+import DTO.SanphamDTO;
+import java.util.ArrayList;
+
 /**
  *
  * @author Tan Thuan
  */
 public class SanphamBUS {
+ 
+    
+    public boolean themSP(SanphamDTO sp){
+        boolean result = new SanphamDAO().addSanphamDAO(sp);
+        return result;
+    }
+    public boolean xoaSP(SanphamDTO sp){
+        boolean result = new SanphamDAO().deleteSanphamDAO(sp);
+        return result;
+    }
+    public boolean suaSP(SanphamDTO sp){
+        boolean result = new SanphamDAO().editSanphamDAO(sp);
+        return result;
+    }
+    public ArrayList<SanphamDTO> getList(){
+        ArrayList<SanphamDTO> listSP= new SanphamDAO().readSanphamDAO();
+        
+        return listSP;
+    }
     
 }
